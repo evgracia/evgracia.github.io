a784b62c6c52caa90c22703972d0acb4ce25b17d
evgracia <evamarina.gh@hotmail.com>
2020-12-11 23:29:09 +0100

    first commit - test

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    fill(0);
+    noStroke();
+    translate(width/2, height/2);
+    ellipse(0, 0, 50, 50);
+  }
